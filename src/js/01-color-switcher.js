c8de3f4451ebb5394131a0771bb0916f6aa19f50
Yaroslav Drohoziuk <exmahinator@ukr.net>
2022-08-26 17:29:58 +0300

    Task 01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtnRef = document.querySelector('[data-start]');
+const stopBtnRef = document.querySelector('[data-stop]');
+let timerId = null;
+
+stopBtnRef.disabled = true;
+
+startBtnRef.addEventListener('click', startAction);
+stopBtnRef.addEventListener('click', stopAction);
+
+function startAction() {
+  startBtnRef.disabled = true;
+  stopBtnRef.disabled = false;
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopAction() {
+  stopBtnRef.disabled = true;
+  startBtnRef.disabled = false;
+  clearInterval(timerId);
+}
