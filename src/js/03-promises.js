34ed0ab5dbfbfb9dad789c0ef7089150363c8548
Yaroslav Drohoziuk <exmahinator@ukr.net>
2022-08-27 20:18:40 +0300

    Task 03 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,36 +1,54 @@
 import Notiflix from 'notiflix';
 
+// --------------------------------------------------------------------------------------------------------
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
 
 // ---------------------------------------------------------------------------------------------------------
 
 const formRef = document.querySelector('.form');
+let savedData = {};
 
-formRef.addEventListener('input', onEnter);
+formRef.addEventListener('input', onInput);
+formRef.addEventListener('submit', onSubmit);
 
-function onEnter(event) {
-  console.log(event.target);
+function onInput(event) {
+  const { name, value } = event.target;
+  savedData[name] = Number(value);
 }
 
-function onClick() {
-  console.log("Hey! I'm clicking!");
+function onSubmit(event) {
+  event.preventDefault();
+  const { firstDelay, delay, position } = savedData;
+  for (let i = 0; i < position; i += 1) {
+    const timeToNext = firstDelay + delay * i;
+    createPromise(i + 1, timeToNext)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`,
+          {
+            timeout: 3000,
+          }
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`,
+          {
+            timeout: 3000,
+          }
+        );
+      });
+  }
 }
-
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
-//       timeout: 3000,
-//     });
-//   })
-//   .catch(({ position, delay }) => {
-//     Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
-//       timeout: 3000,
-//     });
-//   });
